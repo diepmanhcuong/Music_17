db66f55853b6e29205de40a311eb1f56f3021b1a
diepmanhcuong <diepmanhc0511@gmail.com>
2018-05-30 16:38:11 +0700

    init_home_screen

---
+++ app/src/main/java/com/framgia/music_17/screen/playlistoffline/PlaylistOfflineFragment.java
@@ -0,0 +1,18 @@
+package com.framgia.music_17.screen.playlistoffline;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.framgia.music_17.screen.BaseFragment;
+
+public class PlaylistOfflineFragment extends BaseFragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+            Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
